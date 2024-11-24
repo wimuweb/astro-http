0fd6585c200c74cceb58a405f099f9ab38897b08
wimuweb <wimuweb@gmail.com>
2024-11-23 19:22:51 -0500

    Add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,16 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-import node from '@astrojs/node';
+
+
+import cloudflare from '@astrojs/cloudflare';
+
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
-  output: 'hybrid',
-
-  adapter: node({
-    mode: 'standalone',
-  }),
+  output: 'server',
+  adapter: cloudflare(),
 });
